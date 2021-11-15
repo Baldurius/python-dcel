4ff4a2ca9b721024084e6b64912b1d68ff66fce5
Tom Liebmann <tolipost@web.de>
2021-11-15 13:30:34 +0100

    [+] Add test execution to check.sh script

--- check.sh
+++ check.sh
@@ -2,9 +2,13 @@
 
 SCRIPT_DIR=$(dirname "$0")
 PROJECT_DIR=$SCRIPT_DIR
+TEST_DIR="$PROJECT_DIR/tests"
 
 echo "Running pycodestyle"
 python3 -m pycodestyle $PROJECT_DIR
 
 echo "Running mypy"
 python3 -m mypy $PROJECT_DIR
+
+echo "Running tests"
+python3 -m pytest $TEST_DIR
